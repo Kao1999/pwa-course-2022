94740f3f286346fd222d149e2683e37eb150ea79
Kao1999 <nunkao86@gmail.com>
2022-02-19 09:58:16 +0700

    โอ้เย่ สร้าง Router ได้แยะ

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
+import route from 'color-convert/route';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Router from './Router.js'
+
+// index.html --> index.js --> Router.js --> App.js
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
