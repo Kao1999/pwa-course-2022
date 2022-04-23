bd150cfffe9b0cfd31ed92a1fa9a61f0132d83f2
Kao1999 <nunkao86@gmail.com>
2022-04-24 00:13:58 +0700

    W13 Firebase-Install

--- src/context/FirebaseConfig.js
+++ src/context/FirebaseConfig.js
@@ -1,15 +1,16 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/database';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDOPJ9_X7MbrMXHkai922aLK2NVa7xbGjo",
-  authDomain: "gi472-pwa-course-2022.firebaseapp.com",
-  projectId: "gi472-pwa-course-2022",
-  storageBucket: "gi472-pwa-course-2022.appspot.com",
-  messagingSenderId: "967644458652",
-  appId: "1:967644458652:web:6f71ad83773786006b8759",
-  measurementId: "G-Z7Z74R200B",
-  databaseURL: "https://gi472-pwa-course-2022-default-rtdb.asia-southeast1.firebasedatabase.app"
+  apiKey: "AIzaSyCPYcp9CGp51nnh5S_nSORNVd_cjYO8Rcc",
+  authDomain: "pwa-coures-2022.firebaseapp.com",
+  projectId: "pwa-coures-2022",
+  storageBucket: "pwa-coures-2022.appspot.com",
+  messagingSenderId: "1055519115123",
+  appId: "1:1055519115123:web:69e4df64bbcd6c718ea706",
+  measurementId: "G-5LTJ5RYYJX",
+  databaseURL: "https://pwa-coures-2022-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 firebase.initializeApp(firebaseConfig);
